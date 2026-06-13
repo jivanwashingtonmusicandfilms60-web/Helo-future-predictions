08c689f9da0a99365d4e156cbe034420b7f27109
jivanwashingtonmusicandfilms60-web <jivanwashingtonmusicandfilms60@gmail.com>
2026-06-13 14:45:15 -0400

    Create Core.py

---
+++ Core.py
@@ -0,0 +1,14 @@
+class HoloCore:
+    def __init__(self):
+        self.past = None
+        self.present = None
+        self.future = None
+
+    def set_past(self, value):
+        self.past = value
+
+    def set_present(self, value):
+        self.present = value
+
+    def set_future(self, value):
+        self.future = value
